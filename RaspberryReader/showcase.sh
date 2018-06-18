e0a3a5f4ae7806c507df1a767556b969a9b3ba69
Jack Buckley <bucklj4@mcmaster.ca>
2018-06-18 14:39:43 +0000

    Added Raspberry Reader

---
+++ RaspberryReader/showcase.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# TEST RASPI 
+
+
+NAME=$1
+
+
+# CLEANUP
+rm -f test.jpg test.txt
+
+
+# ADJUST AUDIO VOLUME
+# Change VOLUME to increase/decrease 0-100%
+VOLUME=100%
+echo "Setting volume to $VOLUME"
+sudo amixer -q sset PCM,0 ${VOLUME}
+
+
+# PLAY SPEECH 
+echo "playing TTS"
+./speech3.sh "Hello future leap students. I am the Raspberry Reader!"
+
+# WAIT FOR BUTTON
+sudo python3 button.py
+# TEST CAMERA
+echo "taking photo"
+raspistill -cfx 128:128 --awb auto -rot 180 -t 500 -o test.jpg
+ls -l test.jpg
+
+# OCR test
+echo "Converting to Text, standby..."
+tesseract test.jpg test
+cat test.txt
+
+# SPEAK TEXT
+textstring=`cat test.txt`
+./speech3.sh $textstring
+
+# FINISH
+./speech3.sh "That's all the text that I could see. Have a good day, $NAME!"
+
+# Run a web server to view photo
+IP=`hostname -I`
+IP=${IP%?}
+echo "To see photo, browse to http://$IP:8080/test.jpg"
+echo "Press Ctrl-C to exit"
+python -m SimpleHTTPServer 8080
+
